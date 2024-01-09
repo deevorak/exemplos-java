e7273763a74d7e884760ab6eefab7ad21b74a852
deevorak <carlosesr.inf@gmail.com>
2024-01-09 17:15:11 -0300

    exemplos java streams e lambdas

---
+++ src/test/java/br/com/rocha/exemplosjava/ExemplosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocha.exemplosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
